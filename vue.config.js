58c6bbaec68e35a18f19ec446253a049d79503f8
nicholasburka <nburka@mac.com>
2021-06-08 18:10:08 -0400

    public path fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/fileshare-frontend/dist/index.html' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/fileshare-frontend/dist/' : '/'
 }
